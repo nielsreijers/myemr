87dc9e6d6318e67e6eab909fc5ac6988c7a5f71c
Niels Reijers <nielsreijers@gmail.com>
2020-09-08 12:23:40 +0800

    Make mysql address configurable so we can set it to something different from localhost for WSL2

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/mysql"
@@ -18,6 +19,13 @@ func MyPort() (string, error) {
 }
 
 func ConnectGORM() (*gorm.DB, error) {
-	dsn := "root:1234@tcp(localhost:3306)/myemr?parseTime=true"
+	addr := os.Getenv("MYSQL")
+	if addr == "" {
+		addr = "localhost"
+	} else {
+		fmt.Printf("Connecting to %s\n", addr)
+	}
+
+	dsn := "root:1234@tcp(" + addr + ":3306)/myemr?parseTime=true"
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
