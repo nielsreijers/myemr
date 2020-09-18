bb9fb3ddefc554d9ad381c610e35d5b271d2e52b
Niels Reijers <nielsreijers@gmail.com>
2020-09-18 17:54:11 +0800

    added another text copy step

--- web/db/initdb.go
+++ web/db/initdb.go
@@ -35,5 +35,16 @@ func initDbIfEmpty() {
 		step.Data = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 		step.Template = "step_copytext.tmpl.html"
 		db.Create(&step)
+
+		step := Step{}
+		step.Number = 8
+		step.Type = "text copy"
+		step.Data = "Monkey takes selfies after stealing student's phone
+        
+		A Malaysian student whose cellphone was stolen while he was sleeping has tracked down the culprit: a monkey who took photo and video selfies with the device before abandoning it. Zackrydz Rodzi, 20, on Wednesday said that his mobile phone was missing from his bedroom when he woke up on Saturday. He found the phone's casing under his bed, but there was no sign of robbery in his house in Johor state.
+		When his father saw a monkey the next day, he searched in the jungle behind his house. Using his brother's cellphone to call his own device, he found it covered in mud under a palm tree.
+		However, a bigger surprise came when he found a series of monkey selfies and videos recorded in the phone. \"My uncle was joking that maybe the monkey took some selfies with the phone... So when I checked my phone picture gallery, I was shocked. The suspect's face was plastered on the screen,\" Zackrydz said. \"It was hilarious,\" he added."
+		db.Create(&step)
+
 	}
 }
