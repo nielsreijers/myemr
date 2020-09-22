55c1f58a425bd7f5d46a15880fe39bba9f25dced
Niels Reijers <nielsreijers@gmail.com>
2020-09-22 18:42:31 +0800

    Added sql file with some test text copy tasks

---
+++ db/textsteps.sql
@@ -0,0 +1,51 @@
+insert into steps(number, type, template, data) values
+(
+1,
+'text copy',
+'step_copytext.tmpl.html',
+'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+'
+),
+(
+2,
+'text copy',
+'step_copytext.tmpl.html',
+'Monkey takes selfies after stealing student\'s phone
+        
+A Malaysian student whose cellphone was stolen while he was sleeping has tracked down the culprit: a monkey who took photo and video selfies with the device before abandoning it. Zackrydz Rodzi, 20, on Wednesday said that his mobile phone was missing from his bedroom when he woke up on Saturday. He found the phone\'s casing under his bed, but there was no sign of robbery in his house in Johor state.       
+When his father saw a monkey the next day, he searched in the jungle behind his house. Using his brother\'s cellphone to call his own device, he found it covered in mud under a palm tree.      
+However, a bigger surprise came when he found a series of monkey selfies and videos recorded in the phone. "My uncle was joking that maybe the monkey took some selfies with the phone... So when I checked my phone picture gallery, I was shocked. The suspect\'s face was plastered on the screen," Zackrydz said. "It was hilarious," he added.
+'
+),
+(
+3,
+'text copy',
+'step_copytext.tmpl.html',
+'As news reading on social media becomes more and more popular, fake news becomes a major issue concerning the public and government. The fake news can take advantage of multimedia content to mislead readers and get dissemination, which can cause negative effects or even manipulate the public events. One of the unique challenges for fake news detection on social media is how to identify fake news on newly emerged events. Unfortunately, most of the existing approaches can hardly handle this challenge, since they tend to learn event-specific features that can not be transferred to unseen events. In order to address this issue, we propose an end-to-end framework named Event Adversarial Neural Network (EANN), which can derive event-invariant features and thus benefit the detection of fake news on newly arrived events. It consists of three main components: the multi-modal feature extractor, the fake news detector, and the event discriminator. The multi-modal feature extractor is responsible for extracting the textual and visual features from posts. It cooperates with the fake news detector to learn the discriminable representation for the detection of fake news. The role of event discriminator is to remove the event-specific features and keep shared features among events. Extensive experiments are conducted on multimedia datasets collected from Weibo and Twitter. The experimental results show our proposed EANN model can outperform the state-of-the-art methods, and learn transferable feature representations.
+'
+),
+(
+4,
+'text copy',
+'step_copytext.tmpl.html',
+'Escaped alpaca interrupts soccer game in England
+
+An alpaca escaped from a farm in Britain and traveled to a local soccer field, where it interrupted a game just before halftime.
+The game between Carlton Athletic and Ilkley Town in Wakefield, West Yorkshire, England, was paused about five minutes before halftime Saturday when the loose animal made a 20-minute visit to the field.
+Claire Armstrong, wife of Ilkley\'s first team coach and the team\'s COVID-19 officer, captured video of the alpaca being chased around the field by players, coaches and officials.
+Witnesses said the alpaca, named Oscar, was eventually escorted off the field by its owner and play resumed.
+Ilkley went on to win the game 2-0. The team said it is now considering making an alpaca its mascot.
+'
+),
+(
+5,
+'text copy',
+'step_copytext.tmpl.html',
+'Test charter flight from Manila lands at Hengchun
+
+A Philippine charter flight arrived at Pingtung County’s Hengchun Airport yesterday morning as part of a trial run on resuming passenger operations at the facility, the first passenger jet to land at the airport since 2014.
+Pingtung County Commissioner Pan Men-an was at the airport to watch the plane arrive and greet the pilots, albeit from a distance. “This is a historic moment and a milestone,” Pan said.
+“We hope the airline would secure the aviation rights to offer charter flight services, and that more travelers from neighboring countries could arrive via charter flights. In the long run, we plan to seek funding to revamp the airport’s facilities to ensure that it can operate sustainably,” Pan said.
+'
+);
+
